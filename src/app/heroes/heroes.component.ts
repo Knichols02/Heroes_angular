c36e6948c4929e311a51288e061f17a145ccd5e3
Kirsten Nicholson <kirstennic6@gmail.com>
2021-05-12 20:28:27 +0100

    hero component links created and cleaned up code

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,18 +12,12 @@ import { MessageService } from '../message.service.spec';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-  selectedHero? : Hero;
 
 
-  constructor(private heroService: HeroService, private messageService : MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero) : void{
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent : Sleected hero id=${hero.id}');
   }
 
   getHeroes() : void {
